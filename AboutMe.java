19c524a4eddb294ed886af14da8141add7423940
cmerkli <cyntia.merkli592@sccdsb.net>
2020-09-22 14:11:41 -0400

    Added variables

--- AboutMe.java
+++ AboutMe.java
@@ -7,7 +7,9 @@
 public class AboutMe {
     public static void main (String [] args) {
         // Declaring the variables
-        // TODO: Write the code the declares the variables
+        String myName = "Cyntia Merkli";
+        int myAge = 16;
+        double myHeightInMetres = 1.60;
 
         // Print the values
         System.out.println("Hi, my name is " + myName);
@@ -15,10 +17,11 @@ public class AboutMe {
         System.out.println("My height is " + myHeightInMetres + " m.");
 
         // Change the variables
-        // TODO: Write the code that changes the variables
+        myAge = 22;
+        myHeightInMetres = 1.64
 
         // Print the new values
         System.out.println("Some time passes...");
-        // TODO: Write the last two print statements
+        
     }
 }
